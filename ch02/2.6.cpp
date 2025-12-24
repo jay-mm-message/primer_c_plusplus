db5bed1ef5c05cd914c5b6b11d0e15090e649996
jay-mm-message <jay.mm.message@gmail.com>
2025-12-24 09:53:51 +0800

    refactor

--- ch02/2.6.cpp
+++ ch02/2.6.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
 using namespace std;
-
-struct Sales_data {
-	string bookNo;
-	unsigned unit_sold = 0;
-	double revenue = 0.0;
-
-	Sales_data(string no, unsigned sold, double revenue) {
-		this->bookNo = no;
-		this->unit_sold = sold;
-		this->revenue = revenue;
-	}
-
-	void p() {
-		cout << this->bookNo << ' '
-			 << this->unit_sold << ' '
-			 << this->revenue
-			<< endl;
-	}
-};
+#include "Sales_data.h"
 
 int main() {
 	struct Sales_data sdata_1("0-201-78345-x", 3, 20.00);
@@ -26,6 +8,6 @@ int main() {
 
 	sdata_1.p();
 	sdata_2.p();
-	
+
 	return 0;
 }
---
+++ ch02/Sales_data.h
@@ -0,0 +1,20 @@
+#include <iostream>
+
+struct Sales_data {
+	std::string bookNo;
+	unsigned unit_sold = 0;
+	double revenue = 0.0;
+
+	Sales_data(std::string no, unsigned sold, double revenue) {
+		this->bookNo = no;
+		this->unit_sold = sold;
+		this->revenue = revenue;
+	}
+
+	void p() {
+		std::cout << this->bookNo << ' '
+			 << this->unit_sold << ' '
+			 << this->revenue
+			<< std::endl;
+	}
+};
